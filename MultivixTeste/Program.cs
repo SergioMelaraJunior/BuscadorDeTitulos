e1e4b43a475548043a7dff5802ab7eb5a64bdbf9
Sergio Melara Junior <sergio.melara@hotmail.com>
2019-09-16 18:30:02 -0300

    Adicionar arquivos de projeto.

---
+++ MultivixTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultivixTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
